849ef219df649f1aab0595c6b24f079a558b3e6a
Daniel_Melendez_Ramirez <daniel.melendez@correounivalle.edu.co>
2024-05-05 20:52:31 -0500

    Agregando sign in and sign up pages code

--- www/js/index.js
+++ www/js/index.js
@@ -1,29 +1,97 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-
-// Wait for the deviceready event before using any of Cordova's device APIs.
-// See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
 document.addEventListener('deviceready', onDeviceReady, false);
 
 function onDeviceReady() {
-    // Cordova is now initialized. Have fun!
-
-    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
-    document.getElementById('deviceready').classList.add('ready');
+  console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
+  document.getElementById('deviceready').classList.add('ready');
 }
+
+document.addEventListener('init', function (event) {
+
+  var page = event.target;
+
+  if (page.id === 'page1') {
+    page.querySelector('#push-button').onclick = function () {
+      document.querySelector('#myNavigator').pushPage('page2.html',
+        { data: { title: 'Page 2', myData: JSON.stringify({ "first": "primer dato" }) } });
+    };
+  }
+  else if (page.id === 'page2') {
+    page.querySelector('#submit').onclick = async () => {
+      let user_name = page.querySelector('#username').value;
+      let password = page.querySelector('#password').value;
+      let email_address = page.querySelector('#email').value;
+      if(user_name === "" || password === "" || email_address === ""){
+        ons.notification.alert("Debe llenar todos los campos");
+        return;
+      }
+      const User = {
+        user_name,
+        password,
+        email_address
+      }
+      try {
+        const response = await fetch('https://api-todo-list-ta8f.onrender.com/v1/auth/register', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(User)
+        });
+        const data = await response.json();
+        if(response.status === 201){
+          ons.notification.confirm(data.message);
+          page.querySelector('#username').value = '';
+          page.querySelector('#password').value = '';
+          page.querySelector('#email').value = '';
+        }
+        
+      } catch (error) {
+        console.log(error);
+      }
+
+    };
+    page.querySelector('.signIn').onclick = () => {
+      document.querySelector('#myNavigator').pushPage('page3.html');
+    }
+  }
+  else if (page.id === 'page3') {
+    page.querySelector('#submitSignIn').onclick = async () => {
+      let email_address = page.querySelector('#fieldEmail').value;
+      let password = page.querySelector('#fieldPassword').value;
+      if(email_address === "" || password === ""){
+        ons.notification.alert("Debe llenar todos los campos");
+        return;
+      }
+      const User = {
+        email_address,
+        password
+      }
+      try {
+        const response = await fetch('https://api-todo-list-ta8f.onrender.com/v1/auth', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(User)
+        });
+        const data = await response.json();
+        if (response.status === 200) {
+          console.log(data);
+          document.querySelector('#myNavigator').pushPage('page4.html');    
+        }else{
+          ons.notification.alert(data.msg);
+        }
+      } catch (error) {
+        ons.notification.alert("Ups! Algo salió mal");
+      }
+
+    }
+    page.querySelector('.signUp').onclick = () => {
+      document.querySelector('#myNavigator').pushPage('page2.html');
+      console.log("IR");
+    }
+   }
+   else if (page.id === 'page4') {
+    console.log("Hola");
+   }
+});
